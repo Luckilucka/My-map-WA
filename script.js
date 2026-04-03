4a927599f8bd5bcb75b7fcab74bbcc4e8adfcf30
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-03 16:32:39 +0200

    zone tst

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ WA.onInit().then(() => {
 console.log("SCRIPT OK");
 
 WA.room.area.onEnter("npc_bob").subscribe(() => {
+    console.log("ZOne testé");
 WA.ui.openPopup("PNJ", "Bonjour ! 🎉", [
 { label: "ok", callback: popup => popup.close() }
 ]);
