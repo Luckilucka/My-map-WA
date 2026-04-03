6e011f5d9eb12d2d8bc5e62a64369e515715d084
LuluRak <131346065+Luckilucka@users.noreply.github.com>
2026-04-03 16:11:54 +0200

    Changer script

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 WA.onInit().then(() => {
-console.log("Script chargé");
+console.log("SCRIPT OK");
 
 WA.room.area.onEnter("npc_bob").subscribe(() => {
-WA.ui.openPopup("npcPopup", "Bonjour 👋 Je suis un PNJ !", [
-{
-label: "Fermer",
-className: "primary",
-callback: (popup) => {
-popup.close();
-}
-}
+WA.ui.openPopup("PNJ", "Bonjour ! 🎉", [
+{ label: "ok", callback: popup => popup.close() }
 ]);
-});
-
-WA.room.area.onLeave("npc_bob").subscribe(() => {
-WA.ui.closePopup("npcPopup");
 });
 });
